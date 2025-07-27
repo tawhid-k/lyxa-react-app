9715622a29b54816aebd80b7f1b09fdee464a797
tawhid-k <tawhid.khondakar18@gmail.com>
2025-07-27 17:50:53 +0600

    Completed project

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/lyxa-react-app',
-  assetPrefix: '/lyxa-react-app',
-  trailingSlash: true,
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
+  output: 'export',  
+  basePath: process.env.NODE_ENV === 'production' ? '/lyxa-react-app' : '', 
+  trailingSlash: true, 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
